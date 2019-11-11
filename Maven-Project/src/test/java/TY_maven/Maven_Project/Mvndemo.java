0442e2e8944f1132f76aba78bb0c7273bf790f84
sulakshana <sulakshana.sa@gmail.com>
2019-11-11 14:58:51 +0530

    hh

---
+++ Maven-Project/src/test/java/TY_maven/Maven_Project/Mvndemo.java
@@ -0,0 +1,13 @@
+package TY_maven.Maven_Project;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Mvndemo {
+
+	@Test
+	public void mevanproject() {
+		Reporter.log("maven is working",true);
+		//System.out.println("maven is installed");
+	}
+}
